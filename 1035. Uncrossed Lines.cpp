084a036acfee1273b37bf407aa408133fc07340b
Mayur Reddy <54232956+Jack-The-Ripper-1820@users.noreply.github.com>
2023-05-12 14:07:42 -0400

    Update and rename 1035. Uncrossed Lines to 1035. Uncrossed Lines.cpp

--- 1035. Uncrossed Lines.cpp
+++ 1035. Uncrossed Lines.cpp
@@ -1,4 +1,23 @@
 class Solution {
+    int dp[501][501];
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2, int i1 = 0, int i2 = 0) {
+        if(i1 == 0 && i2 == 0) {
+            memset(dp, -1, sizeof(dp));
+        }
+
+        if(i1 >= nums1.size() || i2 >= nums2.size()) return 0;
+
+        if(dp[i1][i2] != -1) return dp[i1][i2];
+        
+        if(nums1[i1] == nums2[i2])
+            return dp[i1][i2] = 1 + maxUncrossedLines(nums1, nums2, i1 + 1, i2 + 1);
+
+        return dp[i1][i2] = max(maxUncrossedLines(nums1, nums2, i1, i2 + 1), maxUncrossedLines(nums1, nums2, i1 + 1, i2));
+    }
+};
+
+class Solution {
     vector<vector<int>> dp;
     
     int helper(vector<int> &s, vector<int> &p, int si, int pi) {
