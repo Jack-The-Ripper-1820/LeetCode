93e45ecba94c5a5879204b4578fea5067c05219d
Mayur Reddy <54232956+Jack-The-Ripper-1820@users.noreply.github.com>
2023-10-19 21:59:38 -0400

    Update and rename 341. Flatten Nested List Iterator to 341. Flatten Nested List Iterator.cpp

--- 341. Flatten Nested List Iterator.cpp
+++ 341. Flatten Nested List Iterator.cpp
@@ -1,51 +1,56 @@
-#python
-# """
-# This is the interface that allows for creating nested lists.
-# You should not implement it, or speculate about its implementation
-# """
-#class NestedInteger:
-#    def isInteger(self) -> bool:
-#        """
-#        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        """
-#
-#    def getInteger(self) -> int:
-#        """
-#        @return the single integer that this NestedInteger holds, if it holds a single integer
-#        Return None if this NestedInteger holds a nested list
-#        """
-#
-#    def getList(self) -> [NestedInteger]:
-#        """
-#        @return the nested list that this NestedInteger holds, if it holds a nested list
-#        Return None if this NestedInteger holds a single integer
-#        """
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
 
-class NestedIterator:
-    def __init__(self, nestedList: [NestedInteger]):
-        self.ans = []
-        self.helper(nestedList)
-        self.n = len(self.ans)
-        self.i = -1
-        
-    def helper(self, l):
-        for i in l:
-            if i.isInteger():
-                self.ans.append(i.getInteger())
-            else:
-                self.helper(i.getList())
-                
-    
-    def next(self) -> int:
-        self.i += 1
-        return self.ans[self.i]
-    
-    def hasNext(self) -> bool:
-         return self.i + 1 < self.n
+class NestedIterator {
+    vector<int> list;
+    int ind = 0;
 
-# Your NestedIterator object will be instantiated and called as such:
-# i, v = NestedIterator(nestedList), []
-# while i.hasNext(): v.append(i.next())
+    void insert(vector<NestedInteger> &nestedList) {
+        for(auto &x : nestedList) {
+            if(x.isInteger()) {
+                list.push_back(x.getInteger());
+                continue;
+            }
+
+            insert(x.getList());
+        }
+    }
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        insert(nestedList);
+    }
+    
+    int next() {
+        return hasNext() ? list[ind++] : -1;
+    }
+    
+    bool hasNext() {
+        return ind < list.size();
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
+
+
 
 //c++
 /**
