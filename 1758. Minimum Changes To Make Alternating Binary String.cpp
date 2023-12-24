312a3272346adab76158ba052a0c5a00d6b999d2
Mayur Reddy <54232956+Jack-The-Ripper-1820@users.noreply.github.com>
2023-12-24 12:53:26 -0500

    Update and rename 1758. Minimum Changes To Make Alternating Binary String1758. Minimum Changes To Make Alternating Binary String to 1758. Minimum Changes To Make Alternating Binary String.cpp

--- 1758. Minimum Changes To Make Alternating Binary String.cpp
+++ 1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -1,4 +1,26 @@
 class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.length();
+        int cnt1 = 0, cnt2 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                cnt1 += s[i] == '1';
+                cnt2 += s[i] == '0';
+            }
+
+            else {
+                cnt1 += s[i] == '0';
+                cnt2 += s[i] == '1';
+            }
+        }
+
+        return min(cnt1, cnt2);
+    }
+};
+
+class Solution {
     int helper(string &s) {
         int ans = 0;
         for(int i = 1; i < s.length(); i++) {
