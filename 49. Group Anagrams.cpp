149d42f7fb1c406c3afffcb8f0cedda6cad7e479
Mayur Reddy <54232956+Jack-The-Ripper-1820@users.noreply.github.com>
2024-02-05 19:49:34 -0500

    Update and rename 49. Group Anagrams to 49. Group Anagrams.cpp

--- 49. Group Anagrams.cpp
+++ 49. Group Anagrams.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<array<int, 26>, vector<string>> mp;
+        
+        for(string &x : strs) {
+            array<int, 26> freq = {0};
+            
+            for(char y : x) freq[y - 'a']++;
+            
+            mp[freq].push_back(x);
+        }
+        
+        vector<vector<string>> ans;
+        for(auto &x : mp) ans.push_back(x.second);
+        
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<vector<int>, vector<string>> mp;
         
         for(string &x : strs) {
