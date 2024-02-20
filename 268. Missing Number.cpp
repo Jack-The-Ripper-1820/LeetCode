f0736abda339498cb0cfd90e12aacbb070fe25ca
Mayur Reddy <54232956+Jack-The-Ripper-1820@users.noreply.github.com>
2024-02-20 10:56:12 -0500

    Update and rename 268. Missing Number to 268. Missing Number.cpp

--- 268. Missing Number.cpp
+++ 268. Missing Number.cpp
@@ -1,3 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int calc_sum = n * (n + 1) / 2;
+        int actual_sum = accumulate(nums.begin(), nums.end(), 0);
+
+        return calc_sum - actual_sum; 
+    }
+};
+
 //O(n) space O(n) time intuitive
 class Solution {
 public:
