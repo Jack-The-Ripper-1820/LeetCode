9a034a432bc681a180c62c7ffcb0c6da8271dee3
Mayur Reddy <54232956+Jack-The-Ripper-1820@users.noreply.github.com>
2024-03-02 14:09:08 -0500

    Update 2864. Maximum Odd Binary Number.cpp

--- 2864. Maximum Odd Binary Number.cpp
+++ 2864. Maximum Odd Binary Number.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
+        int cnt = 0, n = s.length();
+
+        for(char x : s)
+            cnt += x == '1';
+
+        for(int i = 0; i < n; i++) {
+            if(i == n - 1 || cnt > 1) s[i] = '1', cnt--;
+            else s[i] = '0';
+        }
+
+        return s;
+    }
+};
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
         int n = s.length();
         string ans(n, '0');
         int cnt = 0;
