3d1a1e468fac8203080c266b1ea236f5cde0efb7
Mayur Reddy <54232956+Jack-The-Ripper-1820@users.noreply.github.com>
2024-11-06 18:48:03 -0600

    Update 3011. Find if Array Can Be Sorted.cpp

--- 3011. Find if Array Can Be Sorted.cpp
+++ 3011. Find if Array Can Be Sorted.cpp
@@ -1,28 +1,24 @@
 class Solution {
-    void bubble_sort(vector<int>& arr) {
+    bool bubble_sort(vector<int>& arr) {
         int n = arr.size();
         for (int i = 0; i < n - 1; ++i) {
             bool swapped = false;
             for (int j = 0; j < n - i - 1; ++j) {
-                if (arr[j] > arr[j + 1] && __builtin_popcount(arr[j]) == __builtin_popcount(arr[j + 1])) {
-                    swap(arr[j], arr[j + 1]);
-                    swapped = true;
+                if (arr[j] > arr[j + 1]) {
+                    if( __builtin_popcount(arr[j]) == __builtin_popcount(arr[j + 1])) {
+                        swap(arr[j], arr[j + 1]);
+                        swapped = true;
+                    }
+
+                    else return false;
                 }
             }
-
-            if (!swapped) break;
-    }
-}
-public:
-    bool canSortArray(vector<int>& nums) {
-        vector<int> cnums = nums;
-        sort(cnums.begin(), cnums.end());
-        bubble_sort(nums);
-        
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != cnums[i]) return false;
         }
 
         return true;
+    }
+public:
+    bool canSortArray(vector<int>& nums) {
+        return bubble_sort(nums);
     }
 };
